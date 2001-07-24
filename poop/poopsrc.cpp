f3e4c55482163df12b9374f89a7461872ce4dd0e
Nick Welch <mackstann@gmail.com>
2001-07-23 21:24:52 -0500

    File(s) changed at 2001-07-23T21:24:52-05:00
    ./poop/poopsrc.cpp

---
+++ poop/poopsrc.cpp
@@ -0,0 +1,16 @@
+// Listing 6_1
+// Print numbers between 0 and 9
+
+#include <iostream.h>
+
+main()
+{
+	int counter = 0;	//set initial value
+
+	while ( counter < 10) //test condition
+	{
+		cout << "Poop! " << counter << " \n\n"; //display
+		counter++; 			//increment
+	}
+	return(0);
+}
